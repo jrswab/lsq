120d8d98d60956bc2d418c9616bdbe963c0f4025
Jaron Swab <jr@jrswab.com>
2025-05-20 21:52:58 -0400

    prep for v1.3.0 release

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"io"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -17,7 +17,7 @@ import (
 	"github.com/jrswab/lsq/trie"
 )
 
-const semVer string = "1.0.0"
+const semVer string = "1.3.0"
 
 // Search regex pattern in given file, print matching lines
 func searchInFile(filePath string, pattern *regexp.Regexp) error {
